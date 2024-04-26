13fb397644c42d0f8dba6ebd73583a5cf86a4cbb
mikeyjeanson <mikeyjeanson@gmail.com>
2024-04-26 10:14:20 -0600

    Training Hub Made

--- TrainingHub/Deploy/trainingHub.js
+++ TrainingHub/Deploy/trainingHub.js
@@ -1,5 +1,5 @@
 import { html, render } from 'htm/preact'
-import HubCard from './Components/hubCard.js'
+import HubCard from 'webdev/TrainingHub/Components/hubCard.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
